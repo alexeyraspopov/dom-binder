f9def64a39289f63b884c0d0281c9972c8c20603
Avenger7x <avenger7x13@gmail.com>
2014-05-21 19:23:19 +0300

    rewrite

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 'use strict';
 
-function sanitizeValue(value){
-	return value == null ? '' : value;
-}
-
 function compose(f, g){
 	return function(){
 		return f(g.apply(this, arguments));
 	};
 }
 
-function bind(node, attr, observer){
-	var setAttribute = node.setAttribute.bind(node, attr);
+function identity(value){
+	return value;
+}
 
-	observer.subscribe(compose(setAttribute, sanitizeValue));
+function sanitizeValue(value){
+	return value == null ? '' : value;
+}
+
+function valueTransform(attr){
+	switch(attr){
+		case 'checked':
+			return Boolean;
+		case 'value':
+			return String;
+		default:
+			return identity;
+	}
+}
+
+function bind(node, attr, observer){
+	var setAttribute, sanitize;
+
+	setAttribute = node.setAttribute.bind(node, attr);
+	sanitize = compose(valueTransform(attr), sanitizeValue);
+
+	observer.subscribe(compose(setAttribute, sanitize));
 }
 
 module.exports = bind;
